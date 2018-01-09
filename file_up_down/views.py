90d96bf579291ac0c26e3faac3cf555b53a5c54e
loading_21th <loading_21th@163.com>
2018-01-09 19:52:52 +0800

    静态文件 ，ajax跨域post ,文件传送，json解析

--- file_up_down/views.py
+++ file_up_down/views.py
@@ -15,53 +15,28 @@ import json
 
 @csrf_exempt
 def hlsroompage(request,schoolname,classname):
-    print "no"
     if request.method == 'POST':
-        file_obj = request.FILES.getlist('filename')[0]
-        if file_obj:
-            upload_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),'uploadfile',schoolname,classname)
-            if not os.path.exists(upload_path):
-                os.makedirs(upload_path)
+        upload_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),'uploadfile',schoolname,classname)
+        if not os.path.exists(upload_path):
+            os.makedirs(upload_path)
+	if request.FILES:
+            file_obj = request.FILES.getlist('filename')[0]
             with open(os.path.join(upload_path,file_obj.name), 'wb') as newfile:
                 for chunk in file_obj.chunks():
                     newfile.write(chunk)
-	    hlsdic = {'Courseware_name':file_obj.name}
-	    response = JsonResponse(hlsdic, safe=False)
-	    response["Access-Control-Allow-Origin"] = "*"
-	    response["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
-	    response["Access-Control-Max-Age"] = "1000"
-	    response["Access-Control-Allow-Headers"] = "*"
-	    return response
-	    """
-	    # return JsonResponse(hlsdic)
-	    result = str(json.dumps(hlsdic))
-	    mm = request.get('jsonpcallback')
-	    print mm
-	    ll = request.get('jsonpcallback')+'('+result+')'
-	    print ll
-	    return HttpResponse(ll) 
-	    """   
-        else:
-            return HttpResponse(u'Error')
+	hlsdic = {'Courseware_name':os.listdir(upload_path)}
+	response = JsonResponse(hlsdic, safe=False)
+	response["Access-Control-Allow-Origin"] = "*"
+	response["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
+	response["Access-Control-Max-Age"] = "1000"
+	response["Access-Control-Allow-Headers"] = "*"
+	return response
     else:
-        hlsdic = {'schoolname':schoolname,'classname':classname,'num':3,'courseware':'up load your file','Courseware_name':''}
+        hlsdic = {'schoolname':schoolname,'classname':classname,'sum':3}
         return render(request,'index.html',hlsdic);
 
 
 def download(request,schoolname,classname,filename):
-    """
-    def file_iterator(filename,chunk_size=512):
-        basedir = "/home/loading_21th/test_file/"
-        with open(basedir+filename) as f:
-            while true:
-                c = f.read(chunk_size)
-                if c:
-                    yield c
-                else :
-                    break 
-    response = StreamingHttpResponse(file_iterator(filename))
-    """
-    print 'begin'
     fullpath = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),'uploadfile',schoolname,classname,filename)
     ffile = open(fullpath)
     response = FileResponse(ffile)
